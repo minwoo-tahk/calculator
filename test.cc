c42ee2927c387fabf5f45fa205f3f0e9bafdb0ae
탁민우/TOAST기술연구랩/NE <minwoo.tahk@nhnent.com>
2016-08-31 11:52:47 +0900

    First commit for test to GitHub.

---
+++ test.cc
@@ -0,0 +1,15 @@
+/*
+Copyright: NHN Entertainment
+Author: Minwoo Tahk
+Email: minwoo.tahk@nhnent.com
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	cout << "Hello World!" << endl;
+	
+	return 0;
+}
